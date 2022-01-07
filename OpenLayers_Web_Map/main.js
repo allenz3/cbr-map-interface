ba22fc2515d2efbe717278780556fc3e91fdb000
allenz3 <allenz3@uw.edu>
2022-01-07 12:13:13 -0800

    location csv data added, clicked coordinate added

--- OpenLayers_Web_Map/main.js
+++ OpenLayers_Web_Map/main.js
@@ -12,6 +12,41 @@ function init(){
                 source: new ol.source.OSM()
             })
         ],
-        target: "js-map"
-    }) 
+        target: 'js-map'
+    });
+
+    const popupContainerElement = document.getElementById('popup-coordinates');
+    const popup = new ol.Overlay({
+        element: popupContainerElement,
+        positioning: 'center-left'
+    });
+
+    map.addOverlay(popup);
+
+    map.on('click', function(e) {
+        const clickedCoordinate = e.coordinate;
+        popup.setPosition(undefined);
+        popup.setPosition(clickedCoordinate);
+        popupContainerElement.innerHTML = clickedCoordinate;
+    });
 }
+
+getLocation();
+
+async function getLocation() {
+    const response = await fetch('./locations/CV_SacPAS_Proj_LatLon.csv');
+    const data = await response.text();
+    //console.log(data);
+
+    const locations = data.split('\n').slice(1);
+    locations.forEach(location => {
+        const col = location.split(',');
+        const proj = col[0];
+        const name = col[1];
+        const basin = col[2]; 
+        const lat = col[3];
+        const lon = col[4];
+        console.log(proj, name, basin, lat, lon);
+    });
+}
+//https://youtu.be/RfMkdvN-23o
