4e15e17a476598e57cd9dc4edcd0ad356d651f9a
allenz3 <allenz3@uw.edu>
2022-01-13 09:33:52 -0800

    tile layers added

--- OpenLayers_Web_Map/raster.js
+++ OpenLayers_Web_Map/raster.js
@@ -11,7 +11,7 @@ function init() {
             new ol.layer.Tile({
                 source: new ol.source.OSM(),
                 zIndex: 1,
-                visible: true,
+                visible: false,
                 extent: [-13872002.193052245, 5686505.724526227, -13025867.222039266, 6339232.393199415],
                 opacity: 0.5
             }),
@@ -22,12 +22,46 @@ function init() {
     // Layer Group
     const layerGroup = new ol.layer.Group({
         layers: [
-            new ol.layer.Tile({
+            new ol.layer.Tile({ // OpenStreetMap
                 source: new ol.source.OSM({
                     url: 'https://{a-c}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png'
+                    //https://wiki.openstreetmap.org/wiki/Tile_servers
                 }),
                 zIndex: 0,
                 visible: true
+            }),
+            new ol.layer.Tile({ // Bing Maps (key required)
+                source: new ol.source.BingMaps({
+                    key: "Ahi7Plxe8amf9CFyGXySP2KXiTJ9ZENIYigIqIUW0rQ5UVJU5JCnzXbat-LsfQ48",
+                    imagerySet: 'AerialWithLabelsOnDemand'
+                    //https://docs.microsoft.com/en-us/bingmaps/rest-services/imagery/get-imagery-metadata
+                }),
+                visible: false
+            }),
+            new ol.layer.Tile({ // CartoDB
+                source: new ol.source.XYZ({
+                    url: 'https://{1-4}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{scale}.png'
+                    //https://github.com/CartoDB/basemap-styles
+                }),
+                visible: false
+            }),
+            new ol.layer.Tile({ // Tile Debug
+                source: new ol.source.TileDebug(),
+                visible: false
+            }),
+            new ol.layer.Tile({ // Stamen
+                source: new ol.source.Stamen({
+                    layer: 'terrain'
+                    //http://maps.stamen.com/#terrain/12/37.7706/-122.3782
+                }),
+                visible: false
+            }),
+            new ol.layer.Tile ({ // ArcGIS
+                source: new ol.source.TileArcGISRest({
+                    url: 'https://sampleserver1.arcgisonline.com/ArcGIS/rest/services/Demographics/ESRI_Population_World/MapServer'
+                    //https://sampleserver1.arcgisonline.com/ArcGIS/rest/services
+                }),
+                visible: false
             })
         ]
     })
