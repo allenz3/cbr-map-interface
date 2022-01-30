137ae677c80e3ad26581e3c401d4e4010e79bc6c
allenz3 <allenz3@uw.edu>
2022-01-29 19:33:49 -0800

    sources added

--- OpenLayers_Web_Map/csv_to_JSON.py
+++ OpenLayers_Web_Map/csv_to_JSON.py
@@ -30,3 +30,10 @@ with open('./data/locations/CV_SacPAS_Proj_LatLon.csv', 'r') as locations:
             locations["features"].append(location)
         
         json.dump(locations, new_file)
+
+# Sources:
+# https://en.wikipedia.org/wiki/GeoJSON
+# https://stackoverflow.com/questions/1432924/python-change-the-scripts-working-directory-to-the-scripts-own-directory
+# https://youtu.be/q5uM4VKywbA
+# https://www.w3schools.com/python/python_json.asp
+# https://stackoverflow.com/questions/12309269/how-do-i-write-json-data-to-a-file
