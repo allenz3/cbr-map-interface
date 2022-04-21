6b336f8c285136c13b0a8a5881778b7a05abf2e6
allenz3 <allenz3@uw.edu>
2022-04-21 14:01:37 -0700

    refactored location selection code for modularity

--- js/locations.js
+++ js/locations.js
@@ -1,4 +1,8 @@
+import vectorLayers from './vector_layers.js';
+import map from './view.js';
+
 const locationsSet = new Set();
+const selectedLocationsSet = new Set();
 
 // input and save csv location data
 async function getLocation() { // fill the sidebar with location options
@@ -21,7 +25,6 @@ async function getLocation() { // fill the sidebar with location options
     const locationData = await response.json();
     locationData["features"].forEach((feature) => {
         const locationString = feature["properties"]["name"] + " (" + feature["properties"]["proj"] + ")";
-        locationsSet.add(locationString);
         const newElem = document.createElement("option");
         newElem.innerHTML = locationString;
         newElem.className = "location";  
@@ -30,12 +33,26 @@ async function getLocation() { // fill the sidebar with location options
     //return locationsInfo;
 }
 
+// fill set containing all locations
+function fillSet(param) {
+    Object.values(param).forEach(location => locationsSet.add(location));
+}
+setTimeout(fillSet, 500, vectorLayers.source);
+
+// search filter by keyword based on user input
+const deselectAll = document.querySelector(".deselect-all").addEventListener("click", () => {
+    selectedLocationsSet.clear();
+    fillSidebar(selectedLocationsSet);
+    fillPoints(locationsSet, selectedLocationsSet);
+})
+
 // search filter by keyword based on user input
 const search = document.querySelector(".search-filter").addEventListener("input", () => {
     const searchInput = document.querySelector(".search-filter").value;
     const locationsList = document.querySelector(".locationsList");
     locationsList.innerHTML = "";
-    locationsSet.forEach((locationString) => {
+    locationsSet.forEach((location) => {
+        const locationString = location.A.name + " (" + location.A.proj + ")";
         if ((locationString.toLowerCase()).includes(searchInput.toLowerCase())) {
             const newElem = document.createElement("option");
             newElem.innerHTML = locationString;
@@ -44,6 +61,106 @@ const search = document.querySelector(".search-filter").addEventListener("input"
         }
     });
 })
+
+// if location option is clicked
+const locationsList = document.querySelector(".locationsList");
+const selectedLocationsList = document.querySelector(".selectedLocationsList");
+locationsList.addEventListener("click", locationOption => findLocation(locationOption));
+selectedLocationsList.addEventListener("click", locationOption => findLocation(locationOption));
+
+// if a location option is selected
+function findLocation(locationOption) { // argument: option element from location list
+    // prevents event listener from selecting the select element (entire list)
+    if (locationOption.target.tagName === "OPTION" || locationOption.target.tagName === "LI") { // can also use e.target.className === "location"
+        locationsSet.forEach((location) => {
+            const locationString = location.A.name + " (" + location.A.proj + ")";
+            if (locationString === locationOption.target.innerHTML) {
+                if (!selectedLocationsSet.has(location)) {
+                    selectedLocationsSet.add(location);
+                } else {
+                    selectedLocationsSet.delete(location);
+                }
+            }
+        });
+        fillSidebar(selectedLocationsSet);
+        fillPoints(locationsSet, selectedLocationsSet);
+    }
+}
+
+// if a point on the map is clicked
+map.on("singleclick", point => {
+    map.forEachFeatureAtPixel(point.pixel, point => {
+        if (point.getGeometry().getType() === 'Point') {
+            locationsSet.forEach((location) => {
+                if (location.A.proj === point.A.proj) {
+                    if (!selectedLocationsSet.has(location)) { // select point
+                        selectedLocationsSet.add(location);
+                    } else { // deselect point
+                        selectedLocationsSet.delete(location);
+                    }
+                }
+            });
+        }
+    });
+    fillSidebar(selectedLocationsSet);
+    fillPoints(locationsSet, selectedLocationsSet);
+});
+
+// fills the selected locations list sidebar after a location option selection or a mouse click on a point
+function fillSidebar(selectedLocationsSet) {
+    const selectedLocationsList = document.querySelector(".selectedLocationsList");
+    selectedLocationsList.innerHTML = "";
+    selectedLocationsSet.forEach((location) => {
+        if (selectedLocationsSet.has(location)) { // if location is selected put it in the sidebar
+            selectedLocationsSet.add(location);
+            const newElem = document.createElement("li");
+            newElem.innerHTML = location.A.name + " (" + location.A.proj + ")";
+            newElem.className = "selectedLocations"; 
+            document.querySelector(".selectedLocationsList").appendChild(newElem);
+        }
+    });
+    console.log(locationsSet);
+    console.log(selectedLocationsSet);
+}
+
+// fills in the points on the map after a location option selection or a mouse click on a point
+function fillPoints(locationsSet, selectedLocationsSet) {
+    locationsSet.forEach((location) => {
+        if (selectedLocationsSet.has(location)) { // color points blue if selected
+            location.setStyle(
+                new ol.style.Style({
+                    image: new ol.style.Circle({
+                        fill: new ol.style.Fill({
+                            color: [0, 153, 255, 1]
+                        }),
+                        radius: 8,
+                        stroke: new ol.style.Stroke({
+                            color: [0, 0, 0, 1],
+                            width: 2
+                        })
+                    }),
+                    zIndex: 1
+                })
+            )
+        } else { // color points white if not selected
+            location.setStyle(
+                new ol.style.Style({
+                    image: new ol.style.Circle({
+                        fill: new ol.style.Fill({
+                            color: [255, 255, 255, 1]
+                        }),
+                        radius: 8,
+                        stroke: new ol.style.Stroke({
+                            color: [0, 0, 0, 1],
+                            width: 2
+                        })
+                    }),
+                    zIndex: 0
+                })
+            )   
+        }
+    });
+}
 
 export default { getLocation };
 
--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
+import map from './view.js';
 import baseLayerGroup from './base_layers.js';
 import rasterLayerGroup from './raster_layers.js';
 import vectorLayers from './vector_layers.js';
-import controls from './controls.js';
 import interactions from './interactions.js';
 import overlays from './overlays.js';
 import locations from './locations.js';
@@ -9,34 +9,12 @@ import locations from './locations.js';
 
 window.onload = init
 
-const locationsSet = new Set();
-const selectedLocationsSet = new Set();
-
 function init() {
     
-    // initialize map
-    const map = new ol.Map({
-        view: new ol.View({
-            // center: [-13392860.93604214, 6011494.743803331],
-            // center: ol.proj.transform([-120.740135, 47.751076], 'EPSG:4326', 'EPSG:3857'),
-            center: ol.proj.transform([-120.740135, 38.751076], 'EPSG:4326', 'EPSG:3857'),
-            zoom: 7,
-            maxZoom: 16,
-            minZoom: 6,
-            extent: [-14500000,3500000,-12000000,5500000]
-            // rotation: 0.5
-            // https://stackoverflow.com/quiestions/27820784/openlayers-3-center-map
-        }),
-        target: 'js-map',
-        keyboardEventTarget: document,
-        controls: controls
-    });
+    // map.on('click', function(e) {
+    //     console.log(e.coordinate);
+    // });
 
-    map.on('click', function(e) {
-        console.log(e.coordinate);
-    });
-
-    // Views
     map.addInteraction(interactions.dragRotateInteraction);
     // map.addInteraction(interactions.drawInteraction);
 
@@ -50,121 +28,7 @@ function init() {
 
     // create location option selection list
     locations.getLocation();
-
-    // fill set containing all locations
-    function fillSet(param) {
-        Object.values(param).forEach(location => locationsSet.add(location));
-    }
-    setTimeout(fillSet, 500, vectorLayers.source);
-
-    // search filter by keyword based on user input
-    const deselectAll = document.querySelector(".deselect-all").addEventListener("click", () => {
-        selectedLocationsSet.clear();
-        fillSidebar(selectedLocationsSet);
-        fillPoints(locationsSet, selectedLocationsSet);
-    })
-
-    // if location option is clicked
-    const locationsList = document.querySelector(".locationsList");
-    const selectedLocationsList = document.querySelector(".selectedLocationsList");
-    locationsList.addEventListener("click", locationOption => findLocation(locationOption));
-    selectedLocationsList.addEventListener("click", locationOption => findLocation(locationOption));
-
-    // if a location option is selected
-    function findLocation(locationOption) { // argument: option element from location list
-        // prevents event listener from selecting the select element (entire list)
-        if (locationOption.target.tagName === "OPTION" || locationOption.target.tagName === "LI") { // can also use e.target.className === "location"
-            locationsSet.forEach((location) => {
-                const locationString = location.A.name + " (" + location.A.proj + ")";
-                if (locationString === locationOption.target.innerHTML) {
-                    if (!selectedLocationsSet.has(location)) {
-                        selectedLocationsSet.add(location);
-                    } else {
-                        selectedLocationsSet.delete(location);
-                    }
-                }
-            });
-            fillSidebar(selectedLocationsSet);
-            fillPoints(locationsSet, selectedLocationsSet);
-        }
-    }
-
-    // if a point on the map is clicked
-    map.on("singleclick", point => {
-        map.forEachFeatureAtPixel(point.pixel, point => {
-            if (point.getGeometry().getType() === 'Point') {
-                locationsSet.forEach((location) => {
-                    if (location.A.proj === point.A.proj) {
-                        if (!selectedLocationsSet.has(location)) { // select point
-                            selectedLocationsSet.add(location);
-                        } else { // deselect point
-                            selectedLocationsSet.delete(location);
-                        }
-                    }
-                });
-            }
-        });
-        fillSidebar(selectedLocationsSet);
-        fillPoints(locationsSet, selectedLocationsSet);
-    });
-
-    // fills the selected locations list sidebar after a location option selection or a mouse click on a point
-    function fillSidebar(selectedLocationsSet) {
-        const selectedLocationsList = document.querySelector(".selectedLocationsList");
-        selectedLocationsList.innerHTML = "";
-        selectedLocationsSet.forEach((location) => {
-            if (selectedLocationsSet.has(location)) { // if location is selected put it in the sidebar
-                selectedLocationsSet.add(location);
-                const newElem = document.createElement("li");
-                newElem.innerHTML = location.A.name + " (" + location.A.proj + ")";
-                newElem.className = "selectedLocations"; 
-                document.querySelector(".selectedLocationsList").appendChild(newElem);
-            }
-        });
-        console.log(selectedLocationsSet);
-    }
-
-    // fills in the points on the map after a location option selection or a mouse click on a point
-    function fillPoints(locationsSet, selectedLocationsSet) {
-        locationsSet.forEach((location) => {
-            if (selectedLocationsSet.has(location)) { // color points blue if selected
-                location.setStyle(
-                    new ol.style.Style({
-                        image: new ol.style.Circle({
-                            fill: new ol.style.Fill({
-                                color: [0, 153, 255, 1]
-                            }),
-                            radius: 8,
-                            stroke: new ol.style.Stroke({
-                                color: [0, 0, 0, 1],
-                                width: 2
-                            })
-                        }),
-                        zIndex: 1
-                    })
-                )
-            } else { // color points white if not selected
-                location.setStyle(
-                    new ol.style.Style({
-                        image: new ol.style.Circle({
-                            fill: new ol.style.Fill({
-                                color: [255, 255, 255, 1]
-                            }),
-                            radius: 8,
-                            stroke: new ol.style.Stroke({
-                                color: [0, 0, 0, 1],
-                                width: 2
-                            })
-                        }),
-                        zIndex: 0
-                    })
-                )   
-            }
-        });
-    }
 }
-
-export { locationsSet, selectedLocationsSet };
 
 // https://openlayers.org/en/latest/apidoc/module-ol_layer_Vector-VectorLayer.html#getSource
 // https://attacomsian.com/blog/javascript-iterate-objects
